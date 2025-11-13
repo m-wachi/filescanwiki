d0b336ef1ecff1d79030d9685baafae4a6759c2f
m-wachi <xxxx@yyy.zz.com>
2025-11-13 13:27:56 +0900

    add sample code. filescanwiki05a works.

---
+++ scan-doc2-sample.ps1
@@ -0,0 +1,9 @@
+#set PYTHON_PATH=C:\Python27
+#set PYTHON_SCRIPTS=C:\Python27\Scripts
+#set PATH=%PYTHON_PATH%;%PYTHON_SCRIPTS%;%PATH%
+
+cd C:\usr\src\python\filescanwiki
+C:\usr\src\python\filescanwiki\py3env\Scripts\Activate.ps1
+
+python filescanwiki05a.py -l dirlist-sample.txt
+
